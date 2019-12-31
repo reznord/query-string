7380913df515a816c75b4c63e7730698832d8ba0
Anup <allamsetty.anup@gmail.com>
2019-12-31 09:44:47 +0530

    Update references in index.js

--- index.d.ts
+++ index.d.ts
@@ -145,7 +145,7 @@ export function parseUrl(url: string, options?: ParseOptions): ParsedUrl;
 
 export interface StringifyOptions {
 	/**
-	Strictly encode URI components with [`strict-uri-encode`](https://github.com/kevva/strict-uri-encode). It uses [`encodeURIComponent`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent) if set to `false`. You probably [don't care](https://github.com/sindresorhus/query-string/issues/42) about this option.
+	Strictly encode URI components with [`strict-uri-encode`](https://github.com/reznord/strict-uri-encode). It uses [`encodeURIComponent`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent) if set to `false`. You probably [don't care](https://github.com/sindresorhus/query-string/issues/42) about this option.
 
 	@default true
 	*/
